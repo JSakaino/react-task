a98c4ebd9d5f4a5debd5e459999706ec31de0d03
JSakaino <jaimex2177@gmail.com>
2022-08-16 17:16:00 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  return (
+    <main className="bg-zinc-900 h-screen">
+      <div className="container  mx-auto p-10">
+        <TaskForm />
+        <TaskList />
+      </div>
+    </main>
+  );
+}
+
+export default App;
